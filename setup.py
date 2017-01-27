a71f5da7dc80a0bef66bb3dc7a4a591983fe2e06
Gabe Fierro <gtfierro225@gmail.com>
2017-01-26 17:13:12 -0800

    new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bw2dataclient',
-      version='0.1',
+      version='0.3',
       description='Simple wrapper for accessing data resources over BOSSWAVE',
       url='https://github.com/gtfierro/pybw2dataclient',
       author='Gabe Fierro',
