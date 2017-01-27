72ada48f39da857dfc3cf81a49a79031a47ce921
Gabe Fierro <gtfierro225@gmail.com>
2017-01-26 16:13:52 -0800

    examples!

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-import client
+from client import BW2DataClient
+from client import timestamp
 
-c = client.BW2DataClient(client=None, archivers=["ucberkeley"])
+c = BW2DataClient(client=None, archivers=["ucberkeley"])
 
 c.query("select *")
+
+uuids = c.uuids('path like "tedmain"')
+#print c.data_uuids(uuids, "now", "now -10sec")
+#print c.stats('path like "tedmain"', "now", "now -5min", 38)
+print c.window_uuids(uuids, "now", "now -1d", "8h")
